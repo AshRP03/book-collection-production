8a60ca3287caab29ec424148753098e2b9423765
AshRP03 <ashwinrp03@gmail.com>
2025-09-15 23:11:51 -0500

    Book collection 3 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  resources :user_books
+  resources :users
+  root 'user_books#index'
+
   resources :books do
-    member {get 'confirm_delete'}
+    member do
+      get :delete
+    end
   end
-  root 'books#index'
+  
 end
